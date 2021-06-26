8c45e18fbe60324e5d50cbd69464bbbdbb277617
joshuajerryjimpan <j.pan.0923551124@gmail.com>
2021-06-26 21:07:43 +0800

    fifth commit

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -6,7 +6,17 @@ public class Tester {
     public static void main(String[] args) {
         System.out.println("Please enter your parking hours: ");
         Scanner scan = new Scanner(System.in);
-        int parkingHours = scan.nextInt();
-        System.out.printf("You have already parked for " + parkingHours + " hours");
+        int Hours = scan.nextInt();
+        int Fee = 0;
+        if (Hours <= 2){
+            Fee = Fee + Hours * 30;
+        }else if (Hours > 2 && Hours <= 5){
+            Fee = Fee + (Hours-2) * 40 + 2 * 30;
+        }else if (Hours > 5 && Hours <= 9){
+            Fee = Fee + (Hours-5) * 50 + 3 * 40 + 2 * 30;
+        }else if (Hours > 9){
+            Fee = Fee + (Hours-9) * 60 + 4 * 50 + 3 * 40 + 2 * 30;
+        }
+        System.out.println("You have already parked for " + Hours + " hours." + "\n" +"Total is " + Fee);
     }
 }
