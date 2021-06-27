be94f6e64ad6e157158971a13eaf3331b3010aad
joshuajerryjimpan <j.pan.0923551124@gmail.com>
2021-06-27 20:04:16 +0800

    ninth commit

--- src/main/java/com/fju/Date.java
+++ src/main/java/com/fju/Date.java
@@ -1,15 +1,26 @@
 package com.fju;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class Date {
     public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();
-        System.out.println(c.getTime());
-        System.out.println("Pleass enter the time");
-        Scanner scan = new Scanner(System.in);
-        String time = scan.nextLine();
-        System.out.println(time);
+        System.out.println("Please enter the time:" );
+        Scanner sc = new Scanner(System.in);
+        String hours = sc.nextLine();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/HH:mm");
+        java.util.Date current = new java.util.Date();
+        String str = sdf.format(current);
+        try {
+            java.util.Date d1 = sdf.parse(hours);
+            java.util.Date d2 = sdf.parse(str);
+            long dif = d2.getTime()- d1.getTime();
+            long difhr = dif/(60*60*1000);
+            System.out.println(difhr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -1,17 +1,11 @@
 package com.fju;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Calendar;
+
 import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/HH:mm");
         System.out.println("Pleass enter the time");
         Scanner scan = new Scanner(System.in);
-        String time = scan.nextLine();
-        Calendar c = Calendar.getInstance();
         int Hours = scan.nextInt();
         int Fee = 0;
         if (Hours <= 2){
